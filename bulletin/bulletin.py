0679353fc508ff0a37fe0ba05f122d9b706c0207
Sho <everything2me@outlook.com>
2016-04-17 21:41:00 +0900

    Added files via upload
    入力した文字が流れていくやつ
    流せる文字
    a-z, (スペース)

---
+++ bulletin/bulletin.py
@@ -0,0 +1,98 @@
+# -*- coding:utf-8 -*-
+import os
+import time
+import sys
+
+command = ''
+
+class Characters():
+    a = (' ***','*  *','****','*  *','*  *')
+    b = ('*** ','* * ','****','*  *','****')
+    c = ('*** ','*   ','*   ','*   ','****')
+    d = ('*** ','*  *','*  *','*  *','****')
+    e = ('****','*   ','*** ','*   ','****')
+    f = ('****','*   ','*** ','*   ','*   ')
+    g = ('****','*   ','* **','*  *','****')
+    h = ('*  *','*  *','****','*  *','*  *')
+    i = ('****','  * ','  * ','  * ','****')
+    j = (' ***','   *','   *','   *','****')
+    k = ('*  *','*  *','*** ','*  *','*  *')
+    l = ('*   ','*   ','*   ','*   ','****')
+    m = ('*****','* * *','* * *','*   *','*   *')
+    n = ('*  *','** *','* **','* **','*  *')
+    o = ('****','*  *','*  *','*  *','****')
+    p = ('****','*  *','****','*   ','*   ')
+    q = ('****','*  *','*  *','* * ','** *')
+    r = ('****','*  *','*** ','*  *','*  *')
+    s = ('****','*   ','****','   *','****')
+    t = ('****','  * ','  * ','  * ','  * ')
+    u = ('*  *','*  *','*  *','*  *',' ***')
+    v = ('*  *','*  *','* * ','* * ','**  ')
+    w = ('*   *','*   *','* * *','* * *','*****')
+    x = ('*  *','*  *',' ** ','*  *','*  *')
+    y = ('*  *','*  *','****','  * ','  * ')
+    z = ('****','  **',' ** ','**  ','****')
+    whitespace = ('    ','    ','    ','    ','    ')
+    space = (' ',' ',' ',' ',' ')
+    
+class Make_str(Characters):
+    def __init__(self, string):
+        self.string = list(string)
+        self.script = """
+self.first = self.first + Characters.{0}[0]
+self.second = self.second + Characters.{1}[1]
+self.third = self.third + Characters.{2}[2]
+self.fourth = self.fourth + Characters.{3}[3]
+self.fifth = self.fifth + Characters.{4}[4]
+        """
+        
+        self.first = ''
+        self.second = ''
+        self.third = ''
+        self.fourth = ''
+        self.fifth = ''
+            
+    def make(self):
+        for char in self.string:
+            if(char != ' '):
+                exec(self.script.format(char, char, char, char, char))
+                exec(self.script.format('space', 'space', 'space', 'space', 'space'))
+            else:
+                exec(self.script.format('whitespace', 'whitespace', 'whitespace', 'whitespace', 'whitespace'))
+                
+        return((self.first, self.second, self.third, self.fourth, self.fifth))
+
+class User:
+    def __init__(self, string):
+        self.string = Make_str(string).make()
+        self.num = 0
+        self.width = 50
+
+    def display(self):
+        stat = True
+        os.system('cls')
+
+        for i in range(0, 5):
+            for j in range(0, self.width):
+                try:
+                    print(self.string[i][self.num+j], end='')
+                except:
+                    print(' ', end='')
+                    stat = False
+
+            print('')
+
+        return(stat)
+
+    def start(self):
+        if(self.display()):
+            self.num += 1
+            time.sleep(0.1)
+            self.start()
+
+while(True):
+    command = input('Now you type a string.\nType q to quit\n>>>')
+    if(command != 'q'):
+        User(command).start()
+    else:
+        sys.exit(0)
